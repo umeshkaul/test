08b4f5ab6d674046b4530eea7e3d98e56238c7bb
UmKa <umeshkaul@gmail.com>
2017-01-13 10:14:22 -0500

    changes

--- learnPython/ex15.py
+++
@@ -1,5 +0,0 @@
-import stdio
-'''pword = input('enter aword: ')
-print(pword)'''
-person = input('Enter your name: ')
-print('Hello', person)
--- shell/ex2.sh
+++ shell/ex2.sh
@@ -2,5 +2,9 @@
 
 #echo $1;
 
-result=`eval $1`
-echo $result;
+result=`eval echo $1`
+#echo $result;
+inl=`eval echo 'inline $1'`
+result2=`eval echo ssh -t tg '$inl'`
+echo $result2
+echo echo ssh -t tg "'""$inl""'"
